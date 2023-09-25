5e824d5df2feabd82db53bcaea919a4c208bc5f7
Rakoski <mastrakoski@gmail.com>
2023-09-25 05:46:42 -0300

    Added main files
    I will start my react project from here. First out, I will do everything in this project based off of the Desafio Ápice Sistemas.pdf file that was given to me to start off this project and maybe make it look a little better
    than how it is detailed, as the instructions were merely what should be in the system and not how they should be done inch by inch. Thus, I have added the main App.js file with the Header component, that currently only has
    the ADMIN text and line. After, it has the dropdowns requested. Then, on the footer of the page, it has the local, plus the date and time of Brasilia, as per request on the document.

---
+++ my-react-app/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import ComboBox from './combobox';
+import './combobox.css';
+import Footer from "./Footer";
+import Header from "./Header";
+import { Route, Routes, BrowserRouter as Router, useNavigate } from 'react-router-dom';
+
+function App() {
+    const [inputValue1, setInputValue1] = useState('Cadastro');
+    const [inputValue2, setInputValue2] = useState('Movimentos');
+    const [inputValue3, setInputValue3] = useState('Relatórios');
+    const listItemWidth = '215px';
+
+    return (
+        <Router>
+            <div>
+                <Header />
+                <div className="combobox-container">
+                    <ComboBox
+                        options={['Bairros', 'Cidades', 'Pessoas', 'Produtos']}
+                        listItemWidth={listItemWidth}
+                        defaultValue={inputValue1}
+                        inputValue={inputValue1}
+                        setInputValue={setInputValue1}
+                    />
+
+                    <ComboBox
+                        options={['Vendas']}
+                        listItemWidth={listItemWidth}
+                        defaultValue={inputValue2}
+                        inputValue={inputValue2}
+                        setInputValue={setInputValue2}
+                    />
+
+                    <ComboBox
+                        options={['Lista de pessoas', 'Lista de vendas']}
+                        listItemWidth={listItemWidth}
+                        defaultValue={inputValue3}
+                        inputValue={inputValue3}
+                        setInputValue={setInputValue3}
+                    />
+
+                    <Routes>
+                        <Route path="/bairros" element={<h1>Oii o krl</h1>} />
+                        <Route path="/cidades" element={<h1>Oii o cu</h1>} />
+                        <Route path="/pessoas" element={<h1>Oii 3</h1>} />
+                        <Route path="/produtos" element={<h1>Oii 4</h1>} />
+                        <Route path="/vendas" element={<h1>vendas</h1>} />
+                        <Route path="/lista de pessoas" element={<h1>lista d pessoa</h1>} />
+                        <Route path="/lista de vendas" element={<h1>lista d venda</h1>} />
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
+    );
+}
+
+export default App;
